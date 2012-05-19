440b37a1d0d2d6fd6d29916cb413c456a1f5c0e9
Kevin Jenkins <kevinj04@gmail.com>
2012-05-19 19:52:01 -0400

    Initial Commit

---
+++ KJGameInputs/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  KJGameInputs
+//
+//  Created by Kevin Jenkins on 5/19/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
